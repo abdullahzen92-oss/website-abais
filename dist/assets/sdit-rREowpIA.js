f50e9f19bf7accb72681e5370c64c463724a2b73
abdullahzen92-oss <abdullahzen92@gmail.com>
2026-04-15 09:46:30 +0700

    hapus file dist dan update struktur project

--- dist/assets/sdit-rREowpIA.js
+++
@@ -1,1 +0,0 @@
-import{s as t}from"./data-manager-CG9Brrkb.js";import{g as e,a,b as o,_ as n,i}from"./chat-D0TGgotQ.js";document.getElementById("navbarSlot").innerHTML=e("sdit");document.getElementById("footerSlot").innerHTML=a();document.getElementById("waSlot").innerHTML=o();t();n(()=>import("./main-CQel7J0W.js"),[]);i();
